c676f419e48e966a54991114d5abb076d0f5a7b2
brianedwards00 <brian_edwards@csu.fullerton.edu>
2020-11-24 23:47:56 -0800

    Updated title image, fixed header, and revised sections

--- src/App.js
+++ src/App.js
@@ -1,55 +1,88 @@
 import './App.css';
 import React from 'react';
-import house from './Images/house.jpg'
-
+import title from './Images/title.jpeg'
 class App extends React.Component{
-  constructor(props) {
-    super(props)
-    this.state={
-      first:''
-    }
-  }
-
-
-  render() {
-    return(
-      <body style={{position:"absolute",background:"lightBlue", width:"100%",height:"1500px"}}>
-      <div style={{height:"75px",background:"#001a08",position:"sticky",top:"0",width:"100%",zIndex:"100"}}>
-        <span style={{fontFamily:"acmeregular",position:"absolute",color:"white",top:"20px",paddingRight:"15px",right:"0",
-        fontSize:"25px"}}>Future</span>
-
-        <span style={{fontFamily:"acmeregular",position:"absolute",color:"white",top:"20px",paddingRight:"115px",right:"0",
-        fontSize:"25px"}}>Present</span>
-
-        <span style={{fontFamily:"acmeregular",position:"absolute",color:"white",top:"20px",paddingRight:"230px",right:"0",
-        fontSize:"25px"}}>Past</span>
-
-        <span style={{fontFamily:"acmeregular",position:"absolute",color:"white",top:"20px",paddingRight:"315px",right:"0",
-        fontSize:"25px"}}>About</span>
-      </div>
-
-      <div style={{position:"absolute",height:"500px", width:"100%",background:"white",top:"75px"}}>
-        <span style={{fontSize:"50px",fontFamily:"melanserif",position:"absolute",outlineStyle:"solid",right:"10%",top:"250px"}}>Brian Edwards</span>
-        <img src={house} alt = "house" style={{width:"500px"}}/>
-      </div>
-
-      <div style={{position:"absolute",outlineStyle:"solid",height:"500px",width:"85%",background:"green", right:"0",top:"575px"}}>
-      <br/>
-        <span style={{fontFamily:"acmeregular",color:"white", paddingLeft:"42.5%", fontSize:"50px"}}>About</span>
-        <span style={{fontFamily:"acmeregular",color:"white", paddingLeft:"42.5%", fontSize:"50px"}}>Past</span>
-        <br/>
-        <span style={{fontFamily:"acmeregular",color:"white", paddingLeft:"42.5%", fontSize:"50px"}}>Present</span>
-        <br/>
-        <span style={{fontFamily:"acmeregular",color:"white", paddingLeft:"42.5%", fontSize:"50px"}}>Future</span>
-        <br/>
-        <span style={{fontFamily:"acmeregular",color:"white", paddingLeft:"42.5%", fontSize:"50px"}}>Contact Me</span>
-        
-      </div>
-      <div style={{position:"absolute",outlineStyle:"solid",height:"500px",background:"blue",width:"15%",top:"575px"}}></div>
-    </body>
-    );
-  }
-  
+constructor(props) {
+super(props)
+this.state={
+first:''
 }
+}
+render() {
+return(
+<div style={{position:"absolute",background:"lightBlue", width:"100%",height:"3000px"}}>
+  <div style={{height:"75px",background:"white",position:"sticky",top:"0",width:"100%",zIndex:"100"}}>
+    
+    
+    <div className="circle">
+      <ul className="inner">
+        <li>
+            <div className="color">
+            </div>
+        </li>
+        <li>
+            <div className="color">
+            </div>
+        </li>
+        <li>
+            <div className="color">
+            </div>
+        </li>
+      </ul>
+    </div>
 
+
+    <a style={{fontFamily:"acmeregular",position:"absolute",color:"black",top:"20px",right:"15px",
+          fontSize:"25px"}}>Future
+    </a>
+    <a style={{fontFamily:"acmeregular",position:"absolute",color:"black",top:"20px",right:"115px",
+          fontSize:"25px"}}>Present
+    </a>
+    <a style={{fontFamily:"acmeregular",position:"absolute",color:"black",top:"20px",right:"230px",
+          fontSize:"25px"}}>Past
+    </a>
+    <a style={{fontFamily:"acmeregular",position:"absolute",color:"black",top:"20px",right:"315px",
+          fontSize:"25px"}}>About
+    </a>
+  </div>
+  <div style={{backgroundImage: `url(${title})`,backgroundPosition:"center", backgroundSize:"cover", position:"absolute",height:"900px", width:"100%"}}>
+    <span style={{fontSize:"50px",fontFamily:"melanserif",position:"absolute",outlineStyle:"solid",right:"10%",top:"250px"}}>
+      Brian Edwards
+    </span>
+  </div>
+  <div style={{position:"absolute",height:"500px",textAlign:"center",width:"50%",top:"975px",left:"25%"}}>
+    <div style={{background:"white",height:"500px"}}>
+      <br/>
+      <span style={{fontFamily:"acmeregular",color:"#033a61", fontSize:"50px"}}>About
+      </span>
+      <section></section>
+      <p style={{fontFamily:"acmeregular",color:"#033a61", fontSize:"1.5vw",verticalAlign:"middle"}}>
+        Perseverant third year Computer Science university student with an exemplary academic performance at 
+        California State University of Fullerton. Seeking to increase my expertise level in Python, JavaScript, 
+        and/or C++ that will allow me to pursue a career currently in artificial intelligence that will positively 
+        affect society. Highly trainable, willing to learn, and having a profound work ethic either working alone 
+        or in a team.
+      </p>
+    </div>
+    <div style={{background:"gray",height:"300px"}}>
+      <span style={{fontFamily:"acmeregular",color:"white", fontSize:"50px"}}>Past
+      </span>
+    </div>
+    <div style={{background:"black",height:"300px"}}>
+      <span style={{fontFamily:"acmeregular",color:"white", fontSize:"50px"}}>Present
+      </span>
+    </div>
+    <div style={{background:"blue",height:"300px"}}>
+      <span style={{fontFamily:"acmeregular",color:"white", fontSize:"50px"}}>Future
+      </span>
+    </div>
+    <div style={{background:"red",height:"300px"}}>
+      <span style={{fontFamily:"acmeregular",color:"white", fontSize:"50px"}}>Contact Me
+      </span>
+    </div>
+  </div>
+</div>
+);
+}
+}
 export default App;
